e1c62e038a7aeda48aef1fb66a4749fd5e2e032a
R3BEL <vasco8990@gmail.com>
2018-11-25 10:50:45 +0700

    retouched details

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
       <div>
         <div className="container">
           <div className="header-hero flex" >
-            <img className="avatar" src={require("./avatar.JPG")}/>
+            <img className="avatar" src={require("./avatar.JPG")} alt="avatar"/>
             <div className="intro">
-              <h2 className="title">Hey, I'm Vasco de Graaff</h2>
-              <h2 className="subtitle">Student</h2>
+              <h1 className="title">Hey, I'm Vasco de Graaff</h1>
+              <h1 className="subtitle">Student</h1>
             </div>
 
           </div>
@@ -28,7 +28,7 @@ class App extends Component {
                 </a>
               </div>
               <div className="tile is-parent">
-                <a className="product-tile tile is-child test box">
+                <a className="product-tile tile is-child test box" href="">
                   <div>
                     <h3 className="title">title</h3>
                     <h3 className="subtitle">subtitle</h3>
@@ -40,11 +40,11 @@ class App extends Component {
               <h2 className="title">Weekly Progress </h2>
               <div className="tile is-ancestor" style={{marginBottom: '1rem'}}>
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-child box">
-                    <div>
+                  <div className=" tile box is-child weekly" style={{margin: '0!important'}}>
+
                       <iframe title="Makerlog Embed" height="300" style={{width:'100%'}} scrolling="no" frameborder="0" allowtransparency="true" src="https://api.getmakerlog.com/users/897/embed"></iframe>
-                    </div>
-                  </a>
+
+                  </div>
                 </div>
               </div>
             </div>
@@ -52,7 +52,7 @@ class App extends Component {
               <h2 className="title">Gallery</h2>
               <div className="tile is-ancestor">
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-child box">
+                  <a className="product-tile tile is-child box" href="">
                     <div>
                       <h3 className="title">title</h3>
                       <h3 className="subtitle">subtitle</h3>
@@ -60,7 +60,7 @@ class App extends Component {
                   </a>
                 </div>
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-child box">
+                  <a className="product-tile tile is-child box" href="">
                     <div>
                       <h3 className="title">title</h3>
                       <h3 className="subtitle">subtitle</h3>
@@ -73,25 +73,31 @@ class App extends Component {
               <h2 className="title">Get in touch</h2>
               <div className="tile is-ancestor">
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-child box ">
-                    <span class="fab fa-twitter icon" style={{fontSize:'3rem'}}></span>
+                  <a href="https://twitter.com/vascodegraaff" className="product-tile tile is-child box twitter">
+                    <span class="fab fa-twitter icon" style={{fontSize:'3rem', color: 'white'}}></span>
                   </a>
                 </div>
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-chile box">
-                    <span class="far fa-envelope icon" style={{fontSize:'3rem'}}></span>
+                  <a href="mailto:vascodegraaff@gmail.com?subject=mail from vascodegraaff.com" className="product-tile tile is-child box mail">
+                    <span class="far fa-envelope icon" style={{fontSize:'3rem', color: 'white'}}></span>
                   </a>
                 </div>
                 <div className="tile is-parent">
-                  <a className="product-tile tile is-chile box">
-                    <span class="fab fa-github icon" style={{fontSize: '3rem'}}></span>
+                  <a href="https://github.com/vascodegraaff" className="product-tile tile is-child box github" >
+                    <span class="fab fa-github icon " style={{fontSize: '3rem', color: 'white'}}></span>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          
+          <footer>
+            <div className="flex justify-center">
+              <img src={require("./logo.png")} alt="logo" style={{height: '6rem', margin: '2rem'}}></img>
+            </div>
+          </footer>
         </div>
+        
+          
       </div>
     );
   }
